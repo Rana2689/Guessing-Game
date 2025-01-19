ff3118fecd33a897001b6b3cd2298cec94b34211
Blas Fernández <141034305+eLiteEs@users.noreply.github.com>
2025-01-19 12:52:30 +0100

    Ahora te avisa si no has introducido un número válido

--- adivinanzas.cpp
+++ adivinanzas.cpp
@@ -9,24 +9,32 @@ int main() {
 
     int secretNumber = rand() % 100 + 1;
     int attempts = 0;
-    int userGuess;
+    string userGuess;
 
     cout << "Welcome to the Guessing Game!" << endl;
     cout << "Try to guess the secret number between 1 and 100  'Be careful not to put a letter instead of a number because the game loops and could slow down your computer'." << endl;
 
     do {
         cout << "Enter your guess: ";
-        cin >> userGuess;
+        getline(cin, userGuess);
+
+        try {
+            stoi(usetGuess);
+        } catch(invalid_argiment&){
+            cout << "You didn't introduced a number." << endl;
+            continue;
+        } 
+
         attempts++;
 
-        if (userGuess < secretNumber) {
+        if (stoi(userGuess) < secretNumber) {
             cout << "Too low. Try again." << endl;
-        } else if (userGuess > secretNumber) {
+        } else if (stoi(userGuess) > secretNumber) {
             cout << "Too high. Try again." << endl;
         } else {
             cout << "Congratulations! You've guessed the secret number in " << attempts << " attempts!" << endl;
         }
-    } while (userGuess != secretNumber);
+    } while (stoi(userGuess) != secretNumber);
 
     return 0;
 }
